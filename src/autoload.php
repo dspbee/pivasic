2e3360854dc2bfdbf49f587f7ce04b5d8c8f7b4b
Igor Sorokin <dspbee@gmail.com>
2015-12-21 14:55:44 +0200

    Update autoload

--- src/autoload.php
+++ src/autoload.php
@@ -5,12 +5,9 @@
 $srcRoot = dirname(__FILE__);
 spl_autoload_register(function ($path) use ($srcRoot) {
     $srcRoot = rtrim($srcRoot, '/') . '/';
-    /**
-     * Delete vendor from path.
-     */
-    $path = explode('\\', $path);
-    array_shift($path);
-    $path = $srcRoot . implode('/', $path) . '.php';
+    $path = str_replace('Dspbee\\Test\\', '', $path);
+    $path = str_replace('Dspbee\\', '', $path);
+    $path = $srcRoot . $path . '.php';
     if (file_exists($path)) {
         require_once $path;
     }
