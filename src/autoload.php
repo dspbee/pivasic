fa53bd60bf029bd3eedb6f34d6345aaab1e7cf8d
Igor Sorokin <dspbee@gmail.com>
2016-01-24 06:26:51 +0200

    Change var name

--- src/autoload.php
+++ src/autoload.php
@@ -4,10 +4,10 @@
  */
 $srcRoot = dirname(__FILE__);
 spl_autoload_register(function ($path) use ($srcRoot) {
-    $srcRoot = rtrim($srcRoot, '/') . '/';
+    $root = rtrim($srcRoot, '/') . '/';
     $path = str_replace('Dspbee\\Test\\', '', $path);
     $path = str_replace('Dspbee\\', '', $path);
-    $path = $srcRoot . $path . '.php';
+    $path = $root . $path . '.php';
     if (file_exists($path)) {
         require_once $path;
     }
