0f0663f860aeb558950307d72f55c7ae1b1d6ad3
Igor Sorokin <dspbee@gmail.com>
2016-04-29 10:13:30 +0300

    Fix code duplication

--- src/Bundle/Common/Bag/CookieBag.php
+++ src/Bundle/Common/Bag/CookieBag.php
@@ -14,58 +14,6 @@ class CookieBag extends ValueBag
     public function __construct()
     {
         parent::__construct([]);
-    }
-
-    /**
-     * @param array $bag
-     */
-    public function add(array $bag = [])
-    {
-        $_COOKIE = array_replace($_COOKIE, $bag);
-    }
-
-    /**
-     * Returns true if the COOKIE parameter is defined.
-     *
-     * @param string $key The key
-     *
-     * @return bool true if the parameter exists, false otherwise
-     */
-    public function has($key)
-    {
-        return array_key_exists($key, $_COOKIE);
-    }
-
-    /**
-     * Returns the COOKIE keys.
-     *
-     * @return array An array of parameter keys
-     */
-    public function keys()
-    {
-        return array_keys($_COOKIE);
-    }
-
-    /**
-     * Returns a COOKIE parameter by name.
-     *
-     * @param string $key
-     * @param mixed|null $default The default value if the parameter key does not exist
-     *
-     * @return mixed|null
-     */
-    public function fetch($key, $default = null)
-    {
-        return $_COOKIE[$key] ?? $default;
-    }
-
-    /**
-     * Returns the number of values.
-     *
-     * @return int
-     */
-    public function count()
-    {
-        return count($_COOKIE);
+        $this->bag = &$_COOKIE;
     }
 }
--- src/Bundle/Common/Bag/EnvBag.php
+++ src/Bundle/Common/Bag/EnvBag.php
@@ -14,58 +14,6 @@ class EnvBag extends ValueBag
     public function __construct()
     {
         parent::__construct([]);
-    }
-
-    /**
-     * @param array $bag
-     */
-    public function add(array $bag = [])
-    {
-        $_ENV = array_replace($_ENV, $bag);
-    }
-
-    /**
-     * Returns true if the ENV parameter is defined.
-     *
-     * @param string $key The key
-     *
-     * @return bool true if the parameter exists, false otherwise
-     */
-    public function has($key)
-    {
-        return array_key_exists($key, $_ENV);
-    }
-
-    /**
-     * Returns the ENV keys.
-     *
-     * @return array An array of parameter keys
-     */
-    public function keys()
-    {
-        return array_keys($_ENV);
-    }
-
-    /**
-     * Returns a ENV parameter by name.
-     *
-     * @param string $key
-     * @param mixed|null $default The default value if the parameter key does not exist
-     *
-     * @return mixed|null
-     */
-    public function fetch($key, $default = null)
-    {
-        return $_ENV[$key] ?? $default;
-    }
-
-    /**
-     * Returns the number of values.
-     *
-     * @return int
-     */
-    public function count()
-    {
-        return count($_ENV);
+        $this->bag = &$_ENV;
     }
 }
